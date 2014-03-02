e8013f83012958f31f1d992b359bb4df5b334fc5
Dave Voyles <Startingline13@hotmail.com>
2014-03-01 16:48:26 -0800

    Initial Commit

---
+++ Super Rawr Type WinPhone8/Super Rawr Type WinPhone8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Super_Rawr_Type_WinPhone8.Resources;
+
+namespace Super_Rawr_Type_WinPhone8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
